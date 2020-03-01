5e06ab3023ca9d0a078717b6e951631066d8b455
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2020-03-01 17:24:34 +0100

    Bump total memory

--- build-js.sh
+++ build-js.sh
@@ -8,48 +8,47 @@ configure_ffmpeg() {
   cd $BUILD_DIR/FFmpeg
   make clean
   emconfigure ./configure \
-    --cc=emcc \
+      --cc=emcc \
     --nm="llvm-nm -g" \
     --ar=emar \
     --cxx=em++ \
-    --objcc=emcc \
     --dep-cc=emcc \
-	--enable-cross-compile \
-	--target-os=none \
-	--arch=x86 \
-	--disable-runtime-cpudetect \
-	--disable-asm \
-	--disable-fast-unaligned \
-	--disable-pthreads \
-	--disable-w32threads \
-	--disable-os2threads \
-	--disable-debug \
-	--disable-stripping \
-	\
-	--disable-all \
-	--enable-ffmpeg \
-	--enable-avcodec \
-	--enable-avformat \
-	--enable-avutil \
-	--enable-swresample \
-	--enable-swscale \
-	--enable-avfilter \
-	--disable-network \
-	--disable-d3d11va \
-	--disable-dxva2 \
-	--disable-vaapi \
-	--disable-vda \
-	--disable-vdpau \
-	--enable-decoder=h264 \
-	--enable-protocol=file \
-	--disable-bzlib \
-	--disable-iconv \
-	--disable-libxcb \
-	--disable-lzma \
-	--disable-sdl \
-	--disable-securetransport \
-	--disable-xlib \
-	--disable-zlib
+    --enable-cross-compile \
+    --target-os=none \
+    --arch=x86 \
+    --disable-runtime-cpudetect \
+    --disable-asm \
+    --disable-fast-unaligned \
+    --disable-pthreads \
+    --disable-w32threads \
+    --disable-os2threads \
+    --disable-debug \
+    --disable-stripping \
+    \
+    --disable-all \
+    --enable-ffmpeg \
+    --enable-avcodec \
+    --enable-avformat \
+    --enable-avutil \
+    --enable-swresample \
+    --enable-swscale \
+    --enable-avfilter \
+    --disable-network \
+    --disable-d3d11va \
+    --disable-dxva2 \
+    --disable-vaapi \
+    --disable-vda \
+    --disable-vdpau \
+    --enable-decoder=h264 \
+    --enable-protocol=file \
+    --disable-bzlib \
+    --disable-iconv \
+    --disable-libxcb \
+    --disable-lzma \
+    --disable-sdl \
+    --disable-securetransport \
+    --disable-xlib \
+    --disable-zlib
 }
 
 make_ffmpeg() {
@@ -64,7 +63,7 @@ build_ffmpegjs() {
   rm dist/ffmpeg-h264.js
   emcc $BUILD_DIR/FFmpeg/ffmpeg.bc \
     -o dist/ffmpeg-h264.js \
-    -Oz \
+    -Os \
     -O3 \
     -s MODULARIZE=1 \
     --closure 1 \
@@ -77,7 +76,7 @@ build_ffmpegjs() {
     -s NO_EXIT_RUNTIME=1 \
     -s EXPORTED_FUNCTIONS='["_avcodec_register_all","_avcodec_find_decoder_by_name","_avcodec_alloc_context3","_avcodec_open2", "_av_init_packet", "_av_frame_alloc", "_av_packet_from_data", "_avcodec_decode_video2", "_avcodec_flush_buffers"]' \
     -s EXTRA_EXPORTED_RUNTIME_METHODS='["FS", "ccall", "getValue", "setValue", "writeArrayToMemory"]' \
-    -s TOTAL_MEMORY=67108864
+    -s TOTAL_MEMORY=134217728
 }
 
 main() {
